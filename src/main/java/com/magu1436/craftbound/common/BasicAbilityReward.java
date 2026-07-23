412f73105e0edce7db6f99e81ca5eb4cf33cf3d8
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-07-24 02:39:35 +0900

    feat: JSONから `amount` を取得するためのユーティリティメソッドを実装

--- src/main/java/com/magu1436/craftbound/common/BasicAbilityReward.java
+++ src/main/java/com/magu1436/craftbound/common/BasicAbilityReward.java
@@ -9,21 +9,59 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.puffish.skillsmod.api.json.JsonElement;
 import net.puffish.skillsmod.api.reward.Reward;
+import net.puffish.skillsmod.api.reward.RewardConfigContext;
 import net.puffish.skillsmod.api.reward.RewardDisposeContext;
 import net.puffish.skillsmod.api.reward.RewardUpdateContext;
+import net.puffish.skillsmod.api.util.Problem;
+import net.puffish.skillsmod.api.util.Result;
 
 /**
  * プレイヤーの基礎能力を変更する報酬の基底クラス
  */
 public abstract class BasicAbilityReward implements Reward {
+    private static final String AMOUNT_KEY = "amount";
     private final double amount;
 
-    BasicAbilityReward(
+    protected BasicAbilityReward(
         double amount
     ) {
         this.amount = amount;
     };
+
+    /**
+     * {@code RewardConfigContext} から報酬量 {@code amount} を取得するためのユーティリティメソッド
+     * @param context 報酬の設定情報
+     * @return 報酬量を含む {@code Result}
+     */
+    protected static Result<Double, Problem> parseJson( RewardConfigContext context ) {
+        JsonElement result = context.getData()
+            .getSuccess()
+            .orElse(null);
+        if (result == null) {
+            return Result.failure(Problem.message("Some error happened on parsing json. CODE: 0"));
+        }
+
+        var json = result
+            .getAsObject()
+            .getSuccess()
+            .orElse(null);
+        
+        if (json == null) {
+            return Result.failure(Problem.message("Some error happened on parsing json. CODE: 1"));
+        }
+
+        Double amount = json
+            .getDouble(AMOUNT_KEY)
+            .getSuccessOrElse(null);
+
+        if (amount == null) {
+            return Result.failure(Problem.message("Some error happened on parsing json. CODE: 2"));
+        }
+
+        return Result.success(amount);
+    }
 
     /**
      * @return 変更する属性
