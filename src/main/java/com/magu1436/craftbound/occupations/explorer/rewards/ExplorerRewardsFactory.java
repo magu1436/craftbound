abb016790a258a04b121bc087fcf6e3a430d2209
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-07-30 14:27:34 +0900

    feat: 遠征歩行スキルの報酬を実装

--- src/main/java/com/magu1436/craftbound/occupations/explorer/rewards/ExplorerRewardsFactory.java
+++ src/main/java/com/magu1436/craftbound/occupations/explorer/rewards/ExplorerRewardsFactory.java
@@ -3,10 +3,14 @@ package com.magu1436.craftbound.occupations.explorer.rewards;
 import java.util.List;
 
 import com.magu1436.craftbound.common.AttributeRewardArgs;
+import com.magu1436.craftbound.common.AttributeReward.Operation;
 import com.magu1436.craftbound.occupations.adventurer.rewards.AdventurerRewardsFactory;
+import com.magu1436.craftbound.registry.CraftboundAttributes;
 
 public class ExplorerRewardsFactory extends AdventurerRewardsFactory {
-    private static final List<AttributeRewardArgs> rewards = List.of();
+    private static final List<AttributeRewardArgs> rewards = List.of(
+        new AttributeRewardArgs("expedition_endurance", CraftboundAttributes.EXPEDITION_ENDURANCE::get, Operation.ADDITION)
+    );
 
     public static void registerRewards() {
         registerRewards(rewards);
