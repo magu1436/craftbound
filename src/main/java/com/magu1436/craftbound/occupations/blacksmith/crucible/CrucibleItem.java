32bcc22a691ed9c0335dd0ad00d66540ebc4134d
magu1436 <hfzoro77@gmail.com>
2026-08-09 18:03:14 +0900

    feat: 無機能アイテムとしてるつぼを実装

---
+++ src/main/java/com/magu1436/craftbound/occupations/blacksmith/crucible/CrucibleItem.java
@@ -0,0 +1,10 @@
+package com.magu1436.craftbound.occupations.blacksmith.crucible;
+
+import net.minecraft.world.item.Item;
+
+public final class CrucibleItem extends Item {
+
+    public CrucibleItem(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/com/magu1436/craftbound/registry/CraftboundItems.java
+++ src/main/java/com/magu1436/craftbound/registry/CraftboundItems.java
@@ -1,16 +1,23 @@
 package com.magu1436.craftbound.registry;
 
 import com.magu1436.craftbound.Craftbound;
+import com.magu1436.craftbound.occupations.blacksmith.crucible.CrucibleItem;
 
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public final class CraftboundItems {
 
     public static final DeferredRegister<Item> ITEMS = 
             DeferredRegister.create(ForgeRegistries.ITEMS, Craftbound.MODID);
+
+    public static final RegistryObject<Item> CRUCIBLE = ITEMS.register(
+            "crucible",
+            () -> new CrucibleItem(new Item.Properties().stacksTo(1))
+    );
     
     public static void register(IEventBus modEventBus) {
         ITEMS.register(modEventBus);
