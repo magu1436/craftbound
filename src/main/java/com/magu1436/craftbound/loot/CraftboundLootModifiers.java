64830ab73bbb79534582ff4ef26588a2ef92003b
magu1436 <hfzoro77@gmail.com>
2026-08-17 11:04:56 +0900

    feat: 共通Loot Modifier登録クラスを追加

---
+++ src/main/java/com/magu1436/craftbound/loot/CraftboundLootModifiers.java
@@ -0,0 +1,23 @@
+package com.magu1436.craftbound.loot;
+
+import com.magu1436.craftbound.Craftbound;
+import com.mojang.serialization.Codec;
+
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+/** Craftbound共通のGlobal Loot Modifier serializerを登録する. */
+public final class CraftboundLootModifiers {
+
+    private static final DeferredRegister<Codec<? extends IGlobalLootModifier>> SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, Craftbound.MODID);
+
+    private CraftboundLootModifiers() {
+    }
+
+    public static void register(IEventBus eventBus) {
+        SERIALIZERS.register(eventBus);
+    }
+}
